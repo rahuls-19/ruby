4541b6f32c6093af2b64ebf8bc3829f78617b403
Rahul kumar <lit2015023@iiita.ac.in>
2017-01-07 02:38:52 +0530

    if-elsif

--- voting.rb
+++ voting.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
-your_age = 19
-if your_age >=18
+your_age = 18
+if your_age >18
 	puts "you can vote"
+elsif your_age == 18
+	puts "https://www.facebook.com"
 else
-	puts "you can't vote"
+	puts "you can't vote" 
 end
