0b284437998277a9fcf50b539b329465770a1019
Rahul kumar <lit2015023@iiita.ac.in>
2017-01-07 23:29:32 +0530

    adding spliting

---
+++ rock.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+rock_bottom = "Indian Institute of Information Technology"
+puts rock_bottom.split
+date = "07/01/2016"
+puts date.split("/")
+puts date[0..4]
+puts date[0...4]
+$end
