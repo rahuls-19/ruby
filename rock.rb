1078219545719364f834f1a6552cec43315d0180
Rahul kumar <lit2015023@iiita.ac.in>
2017-03-16 23:57:21 +0530

    del rock

--- rock.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-rock_bottom = "Indian Institute of Information Technology"
-puts rock_bottom.split
-date = "07/01/2016"
-puts date.split("/")
-puts date[0..5]
-puts date[0...5]
-$end
